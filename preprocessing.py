Fix bug surrounding K score calculation in original code

preprocessing.py
@@ -24,14 +24,15 @@ def load_and_prepare_data(file_path, threshold = 1, max_timestamp=1000):
     # Pooling
     # crop to (480, 480)
     data = data[:, 80:560]
+    data = data[100:380, 100:380]
     
     h = data.shape[0]
     w = data.shape[1]
     # downsample to (40, 40)
     # reshape to (40, 40, 16)
-    data = data.reshape(40, 12, 40, 12)
+    data = data.reshape(40, 7, 40, 7)
     data = data.swapaxes(1, 2)
-    data = data.reshape(40, 40, 144)
+    data = data.reshape(40, 40, 49)
     
     # Create an empty array with object dtype
     pooled_data = np.empty((40, 40), dtype=object)
@@ -90,12 +91,12 @@ def integrate_events_by_fixed_interval(events, interval, H, W):
     while True:
         left = np.where(t >= current_interval)[0][0]
         
-        right_idxs = np.where(t > current_interval + interval)[0]
+        right_idxs = np.where(t >= current_interval + interval)[0]
         if len(right_idxs) == 0:
             right = N-1
         else:
             right = right_idxs[0]
-            
+        
         frames.append(np.expand_dims(integrate_events_segment_to_frame(x, y, p, H, W, left, right), 0))
 
         current_interval += interval
@@ -106,8 +107,11 @@ def integrate_events_by_fixed_interval(events, interval, H, W):
 def play_single_texture_file(file_path):
     data = load_and_prepare_data(file_path, threshold = 2, max_timestamp=1000)
     events = eventize_data(data)
-    frames = integrate_events_by_fixed_interval(events, 2, 40, 40)
+    frames = integrate_events_by_fixed_interval(events, 1, 40, 40)
     print(frames.shape)
+    print(frames.dtype)
+    print(np.min(frames))
+    print(np.max(frames))
     play_frame(frames, save_gif_to="frames.gif")
 
 def ceiling_division(n, d):
@@ -121,8 +125,8 @@ class NeuroTacDataset(Dataset):
         self.max_timestamp = max_timestamp
         self.data = torch.tensor([])
         self.labels = torch.tensor([])
-        self.Load_data()
-
+        self.Load_data()        
+        
     def Load_data(self):
         # pooling
         H = 40
@@ -130,8 +134,8 @@ class NeuroTacDataset(Dataset):
         threshold = 2
         files = os.listdir(self.data_dir)
         data_length = len(files)
-        data = np.zeros((ceiling_division(self.max_timestamp, self.interval), data_length, 2, H, W))
-        labels = np.zeros(data_length)
+        data = np.zeros((ceiling_division(self.max_timestamp, self.interval), data_length, 2, H, W), dtype=np.uint8)
+        labels = np.zeros(data_length, dtype=int)
         idx = 0
         for datapath in files:
             print("index " + str(idx) + ": " + datapath)
@@ -155,14 +159,17 @@ class NeuroTacDataset(Dataset):
         print(self.labels.shape)
 
     def __len__(self):
-        return len(self.data)
+        return self.data.shape[1]
 
     def __getitem__(self, idx):
         return self.data[:, idx, :, :, :], self.labels[idx]  # Return tuple (input, target)
 
 if __name__ == "__main__":
-    dir_path = "/Users/lanceshi/Desktop/DISS/code/Bristol/SNN/SlideS10"
+    
+    dir_path = "/Users/lance-shi/school/Bristol/SNN/SlideS10"
     labels = ['acrylic', 'canvas', 'cotton', 'fashionfabric', 'felt', 'fur', 'mesh', 'nylon', 'wood', 'wool']
-    nt_dataset = NeuroTacDataset(dir_path, labels)
-    torch.save(nt_dataset, "NeuroTacDataset.pth")
-
+    nt_dataset = NeuroTacDataset(dir_path, labels, interval=1)
+    torch.save(nt_dataset, "NeuroTacDataSetUInt8_cropped.pth")
+    '''
+    play_single_texture_file("/Users/lance-shi/school/Bristol/SNN/SlideS10/taps_trial_0_acrylic_events_on")
+    '''

simplesearch.py
@@ -1,9 +1,11 @@
 import torch
 import torch.nn as nn
 import numpy as np
-from spikingjelly.clock_driven import functional, layer, surrogate, neuron
+#from spikingjelly.clock_driven import functional, surrogate, neuron
+from spikingjelly.activation_based import layer, neuron, surrogate
 import matplotlib.pyplot as plt
-
+from preprocessing import NeuroTacDataset
+import gc
 
 def logdet(K):
     s, ld = np.linalg.slogdet(K)
@@ -11,14 +13,15 @@ def logdet(K):
 
 def find_best_neuroncell(trainset):
 
-    search_batchsize = 256
+    search_batchsize = 64
     repeat = 2
-    num_search = 5000
-    timestep = 5
-    #parameter_limit = 5 * 1000 * 1000 # 5 million parameters
+    num_search = 500
+    timestep = 1000
+    group = 10
     
     #trainig data manipulation
-    #pass
+    train_data = torch.utils.data.DataLoader(trainset, batch_size=search_batchsize,
+                                               shuffle=True, pin_memory=False, num_workers=2)
     
     scores = []
     history = []
@@ -27,9 +30,13 @@ def find_best_neuroncell(trainset):
     with torch.no_grad():
         for i in range(num_search):
             #generate random hidden_neurons 
-            hidn = torch.randint(200, 900)
+            #hidn = torch.randint(16, 512, (1,))
+            hidn = torch.randint(100, 900, (1,))
             
-            searchnet = DenseSpikingNN(hidn)
+            searchnet = DenseSpikingNN(hidn.item(), timestep)
+            #searchnet = ConvSpikingNN(hidn, timestep)
+            searchnet.to(torch.device("mps"))
+            
             searchnet.K = np.zeros((search_batchsize, search_batchsize))
             searchnet.num_actfun = 0
             
@@ -40,20 +47,19 @@ def find_best_neuroncell(trainset):
                 batch_num , neuron_num = out.size()
                 x = (out > 0).float()
 
-                #full_matrix = torch.ones((search_batchsize, search_batchsize)).cuda() * neuron_num
-                full_matrix = torch.ones((search_batchsize, search_batchsize)) * neuron_num
+                full_matrix = torch.ones((search_batchsize, search_batchsize), device="mps") * neuron_num
                 sparsity = (x.sum(1)/neuron_num).unsqueeze(1)
-                norm_K = ((sparsity @ (1-sparsity.t())) + ((1-sparsity) @ sparsity.t())) * neuron_num
-                #rescale_factor = torch.div(0.5* torch.ones((search_batchsize, search_batchsize)).cuda(), norm_K+1e-3)
-                rescale_factor = torch.div(0.5* torch.ones((search_batchsize, search_batchsize)), norm_K+1e-3)
+                norm_K = ((sparsity @ (1-sparsity.t())) + ((1-sparsity) @ sparsity.t()))
+                rescale_factor = torch.div(0.5 * torch.ones((search_batchsize, search_batchsize), device="mps"), norm_K+1e-3)
                 K1_0 = (x @ (1 - x.t()))
                 K0_1 = ((1-x) @ x.t())
                 K_total = (full_matrix - rescale_factor * (K0_1 + K1_0))
-
+                
                 searchnet.K = searchnet.K + (K_total.cpu().numpy())
                 searchnet.num_actfun += 1
 
             s = []
+            #for module in searchnet.conv_fc:
             for name, module in searchnet.named_modules():
                 if neuron_type in str(type(module)):
                     module.register_forward_hook(computing_K_eachtime)
@@ -63,15 +69,19 @@ def find_best_neuroncell(trainset):
                 searchnet.num_actfun = 0
                 data_iterator = iter(train_data)
                 inputs, targets = next(data_iterator)
-                #inputs, targets = inputs.cuda(), targets.cuda()
+                inputs, targets = inputs.to(torch.float32), inputs.to(torch.float32)
+                inputs, targets = inputs.to(torch.device("mps")), targets.to(torch.device("mps"))
                 outputs = searchnet(inputs)
+                #print(searchnet.K/ (searchnet.num_actfun))
                 score = logdet(searchnet.K/ (searchnet.num_actfun))
-                print("Hidden layer: " + str(hidn) + "Score: " + str(score))
+                print("" + str(hidn.item()) + ", " + str(score))
                 s.append(score)
 
             scores.append(np.mean(s))
             history.append(hidn)
+            gc.collect()
         
+        print(scores)
         print ("mean / var:", np.mean(scores), np.var(scores))
         print ("max score:", max(scores))
         best_idx = (np.argsort(scores))[-1]
@@ -88,17 +98,28 @@ class DenseSpikingNN(nn.Module):
         # Fully connected layer: Input 1600 -> hidden_neurons
         self.fc1 = nn.Linear(1600, hidden_neurons)
         # Spiking neuron layer after the first FC layer
-        self.lif1 = neuron.LIFNode()  
+        self.lif1 = neuron.LIFNode(v_threshold=1.0, v_reset=0.0, tau= 1.33)  
         
         # Fully connected layer: hidden_neurons -> 11 (output)
         self.fc2 = nn.Linear(hidden_neurons, 11)
         # Spiking neuron layer after the second FC layer
-        self.lif2 = neuron.LIFNode()  
+        self.lif2 = neuron.LIFNode(v_threshold=1.0, v_reset=0.0, tau= 1.33)  
         
         self.timestep = timestep
         
+        nn.init.uniform_(self.fc1.weight, a = 0, b = 1)
+        nn.init.uniform_(self.fc2.weight, a = 0, b = 1)
+        
     def forward(self, x):
-        # Assume x is of shape [batch_size, 1600]
+        # Assume x is of shape [batchsize, T, 2, 40, 40]
+        # transform x into [T, batchsize, 1600]
+        x = x[:, :, 1, :, :]
+        #print(torch.sum(x))
+        x = torch.flatten(x, start_dim=2, end_dim=3)
+        x = torch.swapaxes(x, 0, 1)
+        
+        #print(x.shape)
+        
         for t in range(self.timestep):
             x_single = x[t]
             x_single = self.fc1(x_single)
@@ -106,3 +127,60 @@ class DenseSpikingNN(nn.Module):
             x_single = self.fc2(x_single)
             x_single = self.lif2(x_single)
         return x_single
+
+class ConvSpikingNN(nn.Module):
+    def __init__(self, channels: int, timestep: int):
+        super(ConvSpikingNN, self).__init__()
+        
+        self.timestep = timestep
+        
+        self.conv_fc = nn.Sequential(
+        layer.Conv2d(1, channels, kernel_size=3, padding=1, bias=False),
+        layer.BatchNorm2d(channels),
+        neuron.LIFNode(surrogate_function=surrogate.ATan(), v_threshold=1.0, v_reset=0.0, tau= 1.33),
+        layer.MaxPool2d(2, 2),  # 20 * 20
+
+        layer.Conv2d(channels, channels, kernel_size=3, padding=1, bias=False),
+        layer.BatchNorm2d(channels),
+        neuron.LIFNode(surrogate_function=surrogate.ATan(), v_threshold=1.0, v_reset=0.0, tau= 1.33),
+        layer.MaxPool2d(2, 2),  # 10 * 10
+
+        layer.Flatten(),
+        layer.Linear(channels * 10 * 10, channels * 4 * 4, bias=False),
+        neuron.LIFNode(surrogate_function=surrogate.ATan(), v_threshold=1.0, v_reset=0.0, tau= 1.33),
+
+        layer.Linear(channels * 4 * 4, 11, bias=False),
+        neuron.LIFNode(surrogate_function=surrogate.ATan(), v_threshold=1.0, v_reset=0.0, tau= 1.33),
+        )
+        
+        for m in self.conv_fc:
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_uniform_(m.weight, a =2)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.uniform_(m.weight, 0, 1.0)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+        
+    def forward(self, x: torch.Tensor):
+        # Assume x is of shape [batchsize, T, 2, 40, 40]
+        # transform x into [T, batchsize, 40, 40]
+        x = x[:, :, 1, :, :]
+        x = torch.unsqueeze(x, dim= 2)
+        x = torch.swapaxes(x, 0, 1)
+                
+        for t in range(self.timestep):
+            x_single = x[t]
+            x_single = self.conv_fc(x_single)
+        return x_single
+
+if __name__ == "__main__":
+    with torch.serialization.safe_globals([NeuroTacDataset]):
+        ds = torch.load("/Users/lance-shi/school/NeuroTacDataSetUInt8_cropped.pth", weights_only=False)
+        
+        best_policy = find_best_neuroncell(ds)